09d3e3e76e6420a45c258feea1e9e2823eacd5b2
Choi <legovlrbdj@naver.com>
2022-04-03 19:07:45 +0900

    [MH] 상속에 대한 공부

--- PP01/main.cpp
+++ PP01/main.cpp
@@ -1,10 +1,28 @@
 ﻿#include <iostream>
+#include <string>
+
 using namespace std;
+
+class Player
+{
+public :
+	Player()   // 생성자
+	{
+		cout << "주인공 생성되었습니다" << endl;
+	}
+	~Player()  // 소멸자
+	{
+		cout << "으악" << endl;
+	} 
+private :
+
+};
 
 int main()
 {
-	int a = 2, b = 3;
-	//C++스타일
-	cout << a+b << "kim babo\n\n";
+	
+	Player *player = new Player();
+	delete(player);
+
 	return 0;
 }
